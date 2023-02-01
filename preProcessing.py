829d2f3fd6cd77353f1cb47a3cb9016658f834eb
Alec Drosu <drosualec@gmail.com>
2023-02-01 02:23:06 -0500

    Make changes to Model to allow floats

--- preProcessing.py
+++ preProcessing.py
@@ -1,4 +1,7 @@
 import re
+import datetime
+import time
+import csv
 
 # Open the input file for reading
 with open("Raw Data/Aruba_17/data", "r") as f:
@@ -22,6 +25,15 @@ for line in data:
         device_id = components[2]
         device_status = components[3]
 
+        # Combine date and time into a single timestamp value
+        try:
+            timestamp = datetime.datetime.strptime(f'{date} {time}', '%Y-%m-%d %H:%M:%S.%f')
+            timestamp = int(timestamp.timestamp())
+        except ValueError:
+            # If the time value does not include a fraction of a second, try again without the fraction
+            timestamp = datetime.datetime.strptime(f'{date} {time}', '%Y-%m-%d %H:%M:%S')
+            timestamp = int(timestamp.timestamp())
+
         # Check if the device is a motion sensor (starts with "M")
         if device_id.startswith("M"):
             device_status = re.sub(r'[^ONOFF]', '', device_status)
@@ -29,35 +41,29 @@ for line in data:
                 if len(components) > 4:
                     activity = components[4] if components[4] in activities else None
                     activity_status = components[5] if len(components) > 5 else None
-                    processed_data.append([date, time, device_id, device_status, activity, activity_status])
+                    processed_data.append([timestamp, device_id, device_status, activity, activity_status])
                 else:
-                    processed_data.append([date, time, device_id, device_status])
+                    processed_data.append([timestamp, device_id, device_status])
             else:
-                processed_data.append([date, time, device_id, device_status])
+                processed_data.append([timestamp, device_id, device_status])
         # Check if the device is a door sensor (starts with "D")
         elif device_id.startswith("D"):
             if device_status in ["OPEN", "CLOSE"]:
-                processed_data.append([date, time, device_id, device_status])
+                processed_data.append([timestamp, device_id, device_status])
         # Check if the device is a temperature sensor (starts with "T")
         elif device_id.startswith("T"):
             temperature = re.sub(r'[^\d.]', '', device_status)
             temperature = float(temperature)
-            processed_data.append([date, time, device_id, temperature, "", ""])
+            processed_data.append([timestamp, device_id, temperature])
 # If the line does not start with a date, it must be a temperature reading
     else:
         device_id = components[0]
         temperature = float(components[1])
         processed_data.append([device_id, temperature])
 
-# Write the processed data to a CSV file
-
-with open("Processed Data/Aruba_17/processed_data.csv", "w") as f:
-    f.write("Date,Time,Device ID,Status,Activity,Activity Status\n")
-    for row in processed_data:
-        date = row[0] if len(row) > 0 else ""
-        time = row[1] if len(row) > 1 else ""
-        device_id = row[2] if len(row) > 2 else ""
-        status = row[3] if len(row) > 3 else ""
-        activity = row[4] if len(row) > 4 else ""
-        activity_status = row[5] if len(row) > 5 else ""
-        f.write(f"{date},{time},{device_id},{status},{activity},{activity_status}\n")
+# Write the processed data to a new file
+with open("Processed Data/Aruba_17/processed_data.csv", "w", newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(["Timestamp", "Device ID", "Status", "Activity", "Activity Status"])
+    for data in processed_data:
+        writer.writerow(data)
