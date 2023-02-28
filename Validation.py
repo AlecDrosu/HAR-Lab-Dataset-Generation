d8e23b08c2cec46b4878e749244008e79a693d49
Alec Drosu <drosualec@gmail.com>
2023-02-28 14:18:13 -0500

    Add more comments to explain the code
    Add KLD to the validation script. Add RMSE to the validation script

--- Validation.py
+++ Validation.py
@@ -13,11 +13,34 @@ predicted_dataset = predicted_dataset.head(1000)
 # Calculate MSE and MAE
 mse = mean_squared_error(original_dataset, predicted_dataset)
 mae = mean_absolute_error(original_dataset, predicted_dataset)
+# Create RMSE
+rmse = np.sqrt(mse)
 
 print("MSE:", mse)
 print("MAE:", mae)
+print("RMSE:", rmse)
 
-# Calculate KLD
-kld = entropy(original_dataset.values.flatten(), predicted_dataset.values.flatten())
+# Calculate the KL Divergence. 
+# KL divergence is typically applied to each feature independently; 
+# it is not designed as a covariant feature measure but rather a metric that shows how each feature has diverged independently from the baseline values.
+predicted_dataset.columns = ['Timestamp', 'Device ID', 'Status', 'Activity', 'Activity Status']
+# Calculate the KL Divergence of each feature independently
+kl_divergence = []
+for column in original_dataset.columns:
+    original_column_values = original_dataset[column]
+    predicted_column_values = predicted_dataset[column]
+    kl_divergence.append(entropy(original_column_values, predicted_column_values))
 
-print("KLD:", kld)
+# Format the output as a table
+kl_divergence_table = pd.DataFrame({'Feature': original_dataset.columns, 'KL Divergence': kl_divergence})
+print(kl_divergence_table)
+
+# Plot the KL Divergence for each feature
+import matplotlib.pyplot as plt
+plt.bar(original_dataset.columns, kl_divergence)
+plt.title('KL Divergence for Each Feature')
+plt.xlabel('Feature')
+plt.ylabel('KL Divergence')
+plt.show()
+
+
