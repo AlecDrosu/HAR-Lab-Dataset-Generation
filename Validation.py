25ec4c5b203d91daa225785d625df03065545b23
Alec Drosu <drosualec@gmail.com>
2023-02-28 09:17:49 -0500

    Change loss function and use tensorBoard
    - loss function is bad, fix it
    - Fixed some normalization things

--- Validation.py
+++ Validation.py
@@ -1,13 +1,11 @@
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from scipy.spatial.distance import cosine, pdist
-from dtaidistance import dtw
-from dtaidistance import dtw_visualisation as dtwvis
 import pandas as pd
 from scipy.stats import entropy
 
 original_dataset = pd.read_csv('Processed Data/Aruba_17/processed_data.csv')
-predicted_dataset = pd.read_csv('Predictions/Aruba_17_prediction.csv')
+predicted_dataset = pd.read_csv('Predictions/Aruba_17_prediction.txt', delimiter=',')
 original_dataset = original_dataset.head(1000)
 predicted_dataset = predicted_dataset.head(1000)
 
